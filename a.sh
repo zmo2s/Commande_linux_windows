8f963db68fdb6e8315f7b019ae7a45048b0f14a5
zmo2s <41673755+zmo2s@users.noreply.github.com>
2020-10-30 21:35:27 +0100

    Add files via upload
    openvpn bash Connection user/mdp script

---
+++ a.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo openvpn --config ~/ASTech.ovpn --auth-user-pass test.txt
+
+
+#definir un fichier txt saans les # qui s'apelle test.txt avec user puis a la ligne avec entré #mdp
+
+
+##### fichier test.txt
+
+
+#user
+#mdp
+
